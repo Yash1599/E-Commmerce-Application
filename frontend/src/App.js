4e0e6138943f08d59fdc2515a48589f19274c10f
Yash.Rockmetric <yash.singhania@rockmetric.com>
2020-10-11 15:35:10 +0530

    Header, Footer Setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
-
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import {Container} from 'react-bootstrap';
 
 const  App= ()=> {
   return (
     <>
-      <h1>Welcome to ProShop</h1>
-      </>
+    <Header/>
+    <main className='py-3'>
+      <Container>
+       <h1>Welcome to ProShop</h1>
+      </Container>
+    </main>
+    <Footer/>
+    </>
   );
 }
 
